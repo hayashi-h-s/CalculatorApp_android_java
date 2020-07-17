75f5c69d6344f6512b6bd93b21fd4fe4b5c7f1af
Haya <max9173627@gmail.com>
2020-07-17 16:56:22 +0900

    [fix]=の処理をメソッドでまとめる

--- app/src/main/java/com/haya/calculatorapp/MainActivity.java
+++ app/src/main/java/com/haya/calculatorapp/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private BigDecimal secondNumber;
 
+    private BigDecimal resultNumber;
+
     private int secondNumberIndex;
 
     private char currentOp;
@@ -196,40 +198,7 @@ public class MainActivity extends AppCompatActivity {
                     btDelete.setVisibility(View.VISIBLE);
                     break;
                 case R.id.btEqual:
-                    String secondNumberString = screenContent.substring(secondNumberIndex,screenContent.length());
-
-                    if (secondNumberString.isEmpty()) {
-                        return;
-                    }
-
-                    secondNumber = new BigDecimal(secondNumberString);
-
-                    if (currentOp == '+'){
-                        secondNumber = secondNumber.add(firstNumber);
-                    } else if (currentOp == '-'){
-                        secondNumber = firstNumber.subtract(secondNumber);
-                    } else if (currentOp == '×') {
-                        secondNumber = firstNumber.multiply(secondNumber);
-                    } else if (currentOp == '÷') {
-                        if ( secondNumber.compareTo(BigDecimal.ZERO) == 0 ) {
-                            return;
-                        }
-                        secondNumber = firstNumber.divide(secondNumber, 14, RoundingMode.HALF_UP);
-                    }
-
-//                    String resultNumber = secondNumber.toString();
-                    String result = nf.format(secondNumber);
-
-                    if (result.endsWith(".0")) {
-                        result = result.substring(0, result.length() - 2);
-                    }
-
-                    calculatorScreen.setText(String.valueOf(result));
-
-                    isOpPressed = false;
-                    isEqual = true;
-                    isZero = false;
-
+                    Equal();
                     break;
             }
             if (isEqual) {
@@ -287,5 +256,41 @@ public class MainActivity extends AppCompatActivity {
             isZero = false;
             currentOp = operation;
         }
+    }
+
+    private void Equal(){
+        String secondNumberString = screenContent.substring(secondNumberIndex,screenContent.length());
+
+        if (secondNumberString.isEmpty()) {
+            return;
+        }
+
+        secondNumber = new BigDecimal(secondNumberString);
+
+        if (currentOp == '+'){
+            resultNumber = secondNumber.add(firstNumber);
+        } else if (currentOp == '-'){
+            resultNumber = firstNumber.subtract(secondNumber);
+        } else if (currentOp == '×') {
+            resultNumber = firstNumber.multiply(secondNumber);
+        } else if (currentOp == '÷') {
+            if ( secondNumber.compareTo(BigDecimal.ZERO) == 0 ) {
+                return;
+            }
+            resultNumber = firstNumber.divide(secondNumber, 14, RoundingMode.HALF_UP);
+        }
+
+//                    String resultNumber = secondNumber.toString();
+        String result = nf.format(resultNumber);
+
+        if (result.endsWith(".0")) {
+            result = result.substring(0, result.length() - 2);
+        }
+
+        calculatorScreen.setText(String.valueOf(result));
+
+        isOpPressed = false;
+        isEqual = true;
+        isZero = false;
     }
 }
